b47bea5b1251b0c9449f3adec95e65deaf686357
Youn-Hee Han <yh21.han@gmail.com>
2016-08-11 18:43:59 +0900

    Streamming Server

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass=uk.co.caprica.vlcj.test.direct.DirectTestPlayer -Djna.library.path=/Applications/VLC.app/Contents/MacOS/lib -DfailIfNoTests=false -Dexec.classpathScope=test -Dexec.args="$1"
+mvn exec:java -Dexec.mainClass=koreatech.link.DirectTestPlayer -Djna.library.path=/Applications/VLC.app/Contents/MacOS/lib -DfailIfNoTests=false -Dexec.classpathScope=test -Dexec.args="$1"
